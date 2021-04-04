a5d73346111d9e106da6265097c77d56688cce1b
Vinícius Monteiro <vini.mont.c@gmail.com>
2021-04-04 10:51:09 -0300

    Adiciona e configura a tabela de Specifications no banco

--- src/database/migrations/1617543172104-CreateSpecifications.ts
+++ src/database/migrations/1617543172104-CreateSpecifications.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreateCategories1617303180929 implements MigrationInterface {
+export class CreateSpecifications1617543172104 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "categories",
+        name: "specifications",
         columns: [
           {
             name: "id",
@@ -30,6 +30,6 @@ export class CreateCategories1617303180929 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("categories");
+    await queryRunner.dropTable("specifications");
   }
 }
