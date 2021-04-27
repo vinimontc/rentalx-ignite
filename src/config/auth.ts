9d596f3941f2110cd95800a06823cbb5defe4a06
Vinícius Monteiro <vini.mont.c@gmail.com>
2021-04-27 13:53:05 -0300

    fix: fix refresh token service

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   secret_token: "a7404e03472d943c83c975e8f75746df",
-  expires_in_token: "15m",
+  expires_in_token: "30m",
   secret_refresh_token: "a77c980827b070affd421b8f526baf1e",
   expires_in_refresh_token: "30d",
   expires_refresh_token_days: 20,
